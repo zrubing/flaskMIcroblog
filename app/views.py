c57498bc0957cf33e1fb92679b508ca2757167fe
rubing <rubingem@163.com>
2017-08-14 09:02:49 +0800

    add db

--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
 from app import app
+from flask import render_template
+from .forms import LoginForm
+from flask import flash,redirect
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "hello word"
+    user={'nickname':'haha'}
+    posts=[
+        {
+            'author':{'nickname':'John'},
+            'body':'nice day'
+        },
+        {
+
+            'author':{'nickname':'khan'},
+            'body':'good boy'
+        }]
+    return render_template('index.html',title='home',user=user,posts=posts)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for openID:'+form.openid.data)
+        return redirect('/index')
+    return render_template('login.html',title='Sign In',form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+

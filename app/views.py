c28458dae78622ba2ff0429e184c2c9c8c236d25
rubing <rubingem@163.com>
2017-08-15 17:24:29 +0800

    config 引入login和openid

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from app import app
+from app import app,lm,db,oid
 from flask import render_template
 from .forms import LoginForm
 from flask import flash,redirect
+
 
 @app.route("/")
 @app.route("/index")
@@ -20,6 +21,7 @@ def index():
     return render_template('index.html',title='home',user=user,posts=posts)
 
 @app.route('/login',methods=['GET','POST'])
+@oid.loginhandler
 def login():
     form=LoginForm()
     if form.validate_on_submit():
@@ -27,4 +29,8 @@ def login():
         return redirect('/index')
     return render_template('login.html',title='Sign In',form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
